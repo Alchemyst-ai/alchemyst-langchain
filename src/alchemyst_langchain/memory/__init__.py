ba724f516a2d46ca67b343a72df8679a7f361353
saloni-alchemystai <saloni@getalchemystai.com>
2026-01-20 16:42:48 +0530

    fixed code format

--- src/alchemyst_langchain/memory/__init__.py
+++ src/alchemyst_langchain/memory/__init__.py
@@ -4,8 +4,7 @@ import time
 from typing import Any, Dict, List
 
 from alchemyst_ai import AlchemystAI
-from langchain_core.chat_history import \
-    BaseChatMessageHistory as BaseChatMemory
+from langchain_core.chat_history import BaseChatMessageHistory as BaseChatMemory
 
 
 class AlchemystMemory(BaseChatMemory):
